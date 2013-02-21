06be9e3e7d44cf10d36afb7c0a1c3a1a4f30bf38
Jack Pappas <jackp@server.fake>
2013-02-21 08:17:26 -0500

    Copied the functions from the PatriciaMap module into static members of the PatriciaMap type itself. This produces slightly more efficient code (since we'll be able to remove the module which is compiled as a separate type).

--- ExtCore/Collections.IntMap.fs
+++ ExtCore/Collections.IntMap.fs
@@ -88,6 +88,194 @@ type internal PatriciaMap<'T> =
     | Lf of uint32 * 'T
     | Br of uint32 * uint32 * PatriciaMap<'T> * PatriciaMap<'T>
 
+    //
+    static member TryFind (key, map : PatriciaMap<'T>) =
+        match map with
+        | Empty ->
+            None
+        | Lf (j, x) ->
+            if j = key then Some x
+            else None
+        | Br (p, m, t0, t1) ->
+            PatriciaMap.TryFind (
+                key, (if key <= p then t0 else t1))
+
+    //
+    static member ContainsKey (key, map : PatriciaMap<'T>) =
+        match map with
+        | Empty ->
+            false
+        | Lf (j, _) ->
+            key = j
+        | Br (_, m, t0, t1) ->
+            PatriciaMap.ContainsKey
+                (key, (if zeroBit (key, m) then t0 else t1))
+
+    //
+    static member Count (map : PatriciaMap<'T>) : int =
+        // OPTIMIZE : We can 'strictify' this by adding an accumulator argument, so we'll
+        // only need to use continuations for one side of the tree (left or right child).
+        // We may also be able to eliminate continuations altogether by using an explicit
+        // stack (either an F# list or System.Collections.Generic.Stack<'T>).
+        let rec count (t : PatriciaMap<'T>) cont =
+            match t with
+            | Empty ->
+                cont 0
+            | Lf (_,_) ->
+                cont 1
+            | Br (_, _, left, right) ->
+                count left <| fun leftCount ->
+                count right <| fun rightCount ->
+                    leftCount + rightCount
+                    |> cont
+
+        count map id
+
+    // TODO : This is an experimental version of count which is optimized for performance.
+    // It should work, but we need to do some tests to be sure. Once we're confident it's correct
+    // we'll remove the original 'Count' function and use this once instead.
+    static member FastCount t =
+        match t with
+        | Empty -> 0u
+        | Lf (_,_) -> 1u
+        | Br (_,_,_,_) as t ->
+            // TODO : Run some experiments to determine if this is a good initial capacity,
+            // or if there is a more suitable value.
+            let stack = System.Collections.Generic.Stack (64)
+
+            // Add the initial tree to the stack.
+            stack.Push t
+
+            /// Recursively processes the tree using the mutable stack.
+            let rec fastCount acc =
+                if stack.Count = 0 then acc
+                else
+                    match stack.Pop () with
+                    | Empty ->
+                        fastCount acc
+                    | Lf (_,_) ->
+                        fastCount (acc + 1u)
+                    
+                    (* OPTIMIZATION :   When one or both children of this node are leaves, we handle
+                                        them directly since it's a little faster. *)
+                    | Br (_, _, Lf (_,_), Lf (_,_)) ->
+                        fastCount (acc + 2u)
+                    
+                    | Br (_, _, Lf (_,_), child)
+                    | Br (_, _, child, Lf (_,_)) ->
+                        stack.Push child
+                        fastCount (acc + 1u)
+
+                    | Br (_, _, left, right) ->
+                        // Push both children onto the stack and recurse to process them.
+                        stack.Push left
+                        stack.Push right
+                        fastCount acc
+
+            fastCount 0u
+
+    //
+    static member Remove (key, map : PatriciaMap<'T>) =
+        match map with
+        | Empty ->
+            Empty
+        | Lf (j, _) as t ->
+            if j = key then Empty
+            else t
+        
+        | Br (p, m, t0, t1) as t ->
+            if matchPrefix (key, p, m) then
+                if zeroBit (key, m) then
+                    match PatriciaMap.Remove (key, t0) with
+                    | Empty -> t1
+                    | t0 ->
+                        Br (p, m, t0, t1)
+                else
+                    match PatriciaMap.Remove (key, t1) with
+                    | Empty -> t0
+                    | t1 ->
+                        Br (p, m, t0, t1)
+            else t
+
+    //
+    static member private Join (p0, t0 : PatriciaMap<'T>, p1, t1) =
+        let m = branchingBit (p0, p1)
+        if zeroBit (p0, m) then
+            Br (mask (p0, m), m, t0, t1)
+        else
+            Br (mask (p0, m), m, t1, t0)
+
+    //
+    static member Insert (key, value : 'T, map) =
+        // TODO : Lift this recursive function out into a private member
+        let rec ins = function
+            | Empty ->
+                Lf (key, value)
+            | Lf (j, y) as t ->
+                let newLeaf = Lf (key, value)
+                if j = key then
+                    newLeaf
+                else
+                    PatriciaMap.Join (key, newLeaf, j, t)
+
+            | Br (p, m, t0, t1) as t ->
+                if matchPrefix (key, p, m) then
+                    if zeroBit (key, m) then
+                        Br (p, m, ins t0, t1)
+                    else
+                        Br (p, m, t0, ins t1)
+                else
+                    PatriciaMap.Join (key, Lf (key, value), p, t)
+
+        // Call the implementation function.
+        ins map
+
+    //
+    static member Merge (s, t) : PatriciaMap<'T> =
+        match s, t with
+        | Empty, t
+        | t, Empty ->
+            t
+        | Lf (k, x), t ->
+            PatriciaMap.Insert (k, x, t)
+        | (Br (p, m, s0, s1) as s), Lf (k, x) ->
+            if matchPrefix (k, p, m) then
+                if zeroBit (k, m) then
+                    let left = PatriciaMap.Insert (k, x, s0)
+                    Br (p, m, left, s1)
+                else
+                    let right = PatriciaMap.Insert (k, x, s1)
+                    Br (p, m, s0, right)
+            else
+                PatriciaMap.Join (k, Lf (k, x), p, s)
+
+        | (Br (p, m, s0, s1) as s), (Br (q, n, t0, t1) as r) ->
+            if m = n && p = q then
+                // The trees have the same prefix. Merge the subtrees.
+                let left = PatriciaMap.Merge (s0, t0)
+                let right = PatriciaMap.Merge (s1, t1)
+                Br (p, m, left, right)
+                
+            elif m < n && matchPrefix (q, p, m) then
+                // q contains p. Merge t with a subtree of s.
+                if zeroBit (q, m) then
+                    let left = PatriciaMap.Merge (s0, t)
+                    Br (p, m, left, s1)
+                else
+                    let right = PatriciaMap.Merge (s1, t)
+                    Br (p, m, s0, right)
+
+            elif m > n && matchPrefix (p, q, n) then
+                // p contains q. Merge s with a subtree of t.
+                if zeroBit (p, n) then
+                    let left = PatriciaMap.Merge (s, t0)
+                    Br (q, n, left, t1)
+                else
+                    let right = PatriciaMap.Merge (s, t1)
+                    Br (q, n, t0, right)
+            else
+                // The prefixes disagree.
+                PatriciaMap.Join (p, s, q, t)
 
 //
 [<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
