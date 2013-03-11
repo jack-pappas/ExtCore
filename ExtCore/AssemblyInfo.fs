0f284407415e77e7ff05bf1be699a61663f12344
Jack Pappas <jackp@server.fake>
2013-03-11 16:27:20 -0400

    Incremented version to 0.8.4.

--- ExtCore/AssemblyInfo.fs
+++ ExtCore/AssemblyInfo.fs
@@ -48,9 +48,9 @@ let [<Literal>] private assemblyConfig =
     #endif
 
 // Version information
-[<assembly: AssemblyVersion("0.8.3")>]
-[<assembly: AssemblyFileVersion("0.8.3")>]
-[<assembly: AssemblyInformationalVersion("0.8.3")>]
+[<assembly: AssemblyVersion("0.8.4")>]
+[<assembly: AssemblyFileVersion("0.8.4")>]
+[<assembly: AssemblyInformationalVersion("0.8.4")>]
 
 // Assembly information
 [<assembly: AssemblyTitle("ExtCore")>]
