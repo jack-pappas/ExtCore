568687a65739a43dafe57ffd9154355e581aa7f8
Jack Pappas <jackp@server.fake>
2013-04-09 22:11:29 -0400

    Implemented tests for the String module (not including String.Split).

--- ExtCore.Tests/String.fs
+++ ExtCore.Tests/String.fs
@@ -264,76 +264,527 @@ module Substring =
 /// Tests for the ExtCore.String module.
 module String =
     [<TestCase>]
+    let isEmpty () : unit =
+        // Test case for empty string.
+        String.isEmpty String.empty
+        |> should be True
+
+        // Test case for any other string.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.isEmpty
+        |> should be False
+
+    [<TestCase>]
+    let ofOption () : unit =
+        // Test case for None.
+        String.ofOption None
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for Some containing an empty string.
+        Some String.empty
+        |> String.ofOption
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for Some containing a non-empty string.
+        Some "Hello World!"
+        |> String.ofOption
+        |> should equal "Hello World!"
+
+    [<TestCase>]
+    let ofArray () : unit =
+        // Test case for empty array.
+        Array.empty
+        |> String.ofArray
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for a non-empty array.
+        [| 'H'; 'e'; 'l'; 'l'; 'o'; ' '; 'W'; 'o'; 'r'; 'l'; 'd'; '!'; |]
+        |> String.ofArray
+        |> should equal "Hello World!"
+
+    [<TestCase>]
+    let toOption () : unit =
+        // Test case for null.
+        String.toOption null
+        |> Option.isNone
+        |> should be True
+
+        // Test case for empty string.
+        String.toOption String.empty
+        |> Option.isNone
+        |> should be True
+
+        // Test case for a non-empty string.
+        "Hello World!"
+        |> String.toOption
+        |> should equal (Some "Hello World!")
+
+    [<TestCase>]
+    let toArray () : unit =
+        // Test case for an empty string.
+        String.empty
+        |> String.toArray
+        |> Array.isEmpty
+        |> should be True
+
+        // Test case for a non-empty string.
+        "Hello World!"
+        |> String.toArray
+        |> should equal
+            [| 'H'; 'e'; 'l'; 'l'; 'o'; ' '; 'W'; 'o'; 'r'; 'l'; 'd'; '!'; |]
+
+    [<TestCase>]
+    let split () : unit =
+        // Test case for an empty string.
+        String.empty
+        |> String.split [| ','; |]
+        |> Array.isEmpty
+        |> should be True
+
+        // Test case for a non-empty string which does not contain the specified characters.
+        "Hello World!"
+        |> String.split [| ','; |]
+        |> should equal [| "Hello World!"; |]
+
+        // Test case for a string which does contain the specified characters.
+        "Id,Title,First,MI,Last,DOB"
+        |> String.split [| ','; |]
+        |> should equal
+            [| "Id"; "Title"; "First"; "MI"; "Last"; "DOB"; |]
+
+        // Test case for a string which does contain the specified characters,
+        // and where there are adjacent occurrences of the characters.
+        "3262,,John,Q,Doe,1970-Jan-01"
+        |> String.split [| ','; |]
+        |> should equal
+            [| "3262"; ""; "John"; "Q"; "Doe"; "1970-Jan-01"; |]
+
+    [<TestCase>]
+    let concatArray () : unit =
+        // Test case for empty array.
+        Array.empty
+        |> String.concatArray
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for non-empty array.
+        [| "The"; "quick"; "brown"; "fox"; "jumps"; "over"; "the"; "lazy"; "dog."; |]
+        |> String.concatArray
+        |> should equal
+            "Thequickbrownfoxjumpsoverthelazydog."
+
+    [<TestCase>]
+    let ofLines () : unit =
+        // Test case for empty array.
+        Array.empty
+        |> String.ofLines
+        |> String.isEmpty
+        |> should be True
+
+        // Sample usage test case.
+        [| "Lorem"; "ipsum"; "dolor"; "sit"; "amet"; |]
+        |> String.ofLines
+        |> should equal @"Lorem
+ipsum
+dolor
+sit
+amet"
+
+    [<TestCase>]
+    let toLines () : unit =
+        // Test case for empty string.
+        String.empty
+        |> String.toLines
+        |> Array.isEmpty
+        |> should be True
+
+        // Test case for a single-line string.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.toLines
+        |> should equal
+            [| "The quick brown fox jumps over the lazy dog."; |]
+
+        // Test case for a multi-line string.
+        @"Lorem
+ipsum
+dolor
+sit
+amet"
+        |> String.toLines
+        |> should equal
+            [| "Lorem"; "ipsum"; "dolor"; "sit"; "amet"; |]
+
+    [<TestCase>]
     let sub () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty substring of an empty string.
+        String.sub String.empty 0 0
+        |> ExtCore.Substring.isEmpty
+        |> should be True
+
+        // Test case for empty substring of a non-empty string.
+        String.sub "The quick brown fox jumps over the lazy dog." 4 0
+        |> ExtCore.Substring.toString
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for a non-empty substring of a non-empty string.
+        String.sub "The quick brown fox jumps over the lazy dog." 4 15
+        |> ExtCore.Substring.toString
+        |> should equal "quick brown fox"
 
     [<TestCase>]
     let tryFindIndexOf () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.tryFindIndexOf 'j'
+        |> should equal None
+
+        // Test case for a string which does not contain the specified character.
+        "Hello World!"
+        |> String.tryFindIndexOf 'j'
+        |> should equal None
+
+        // Test case for a string which contains exactly one (1) instance of the specified character.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.tryFindIndexOf 'j'
+        |> should equal (Some 20)
+
+        // Test case for a string which contains multiple instances of the specified character.
+        "Hello World!"
+        |> String.tryFindIndexOf 'l'
+        |> should equal (Some 2)
 
     [<TestCase>]
     let findIndexOf () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for a string which contains exactly one (1) instance of the specified character.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.findIndexOf 'j'
+        |> should equal 20
+
+        // Test case for a string which contains multiple instances of the specified character.
+        "Hello World!"
+        |> String.findIndexOf 'l'
+        |> should equal 2
+
+    [<TestCase; ExpectedException(typeof<System.Collections.Generic.KeyNotFoundException>)>]
+    let ``findIndexOf raises exn when the string does not contain the character`` () : unit =
+        // Test case for a string which does not contain the specified character.
+        "Hello World!"
+        |> String.findIndexOf 'j'
+        |> ignore
 
     [<TestCase>]
     let tryFindIndex () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.tryFindIndex (fun c ->
+            // Is this a vowel (in English)?
+            let c = System.Char.ToLowerInvariant c
+            c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+        |> should equal None
+
+        // Test case for a non-empty string which does not contain a matching character.
+        "Glyndyfrdwy"
+        |> String.tryFindIndex (fun c ->
+            // Is this a vowel (in English)?
+            let c = System.Char.ToLowerInvariant c
+            c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+        |> should equal None
+
+        // Test case for a string which contains exactly one (1) matching character.
+        "Rhythmist"
+        |> String.tryFindIndex (fun c ->
+            // Is this a vowel (in English)?
+            let c = System.Char.ToLowerInvariant c
+            c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+        |> should equal (Some 6)
+
+        // Test case for a string which contains multiple matching characters.
+        "Hello World!"
+        |> String.tryFindIndex (fun c ->
+            // Is this a vowel (in English)?
+            let c = System.Char.ToLowerInvariant c
+            c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+        |> should equal (Some 1)
 
     [<TestCase>]
     let findIndex () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for a string which contains exactly one (1) matching character.
+        "Rhythmist"
+        |> String.findIndex (fun c ->
+            // Is this a vowel (in English)?
+            let c = System.Char.ToLowerInvariant c
+            c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+        |> should equal 6
+
+        // Test case for a string which contains multiple matching characters.
+        "Hello World!"
+        |> String.findIndex (fun c ->
+            // Is this a vowel (in English)?
+            let c = System.Char.ToLowerInvariant c
+            c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+        |> should equal 1
+
+    [<TestCase; ExpectedException(typeof<System.Collections.Generic.KeyNotFoundException>)>]
+    let ``findIndex raises exn when the string does not contain a matching character`` () : unit =
+        // Test case for a non-empty string which does not contain a matching character.
+        "Glyndyfrdwy"
+        |> String.findIndex (fun c ->
+            // Is this a vowel (in English)?
+            let c = System.Char.ToLowerInvariant c
+            c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u')
+        |> ignore
 
     [<TestCase>]
     let fold () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        ((0L, 0), String.empty)
+        ||> String.fold (fun (checksum, index) c ->
+            (checksum + int64 index) * int64 c,
+            index + 1)
+        |> fst  // Discard the index
+        |> should equal 0L
+
+        // Test case for "normal" usage of this function.
+        ((0L, 0), "The quick brown fox jumps over the lazy dog.")
+        ||> String.fold (fun (checksum, index) c ->
+            (checksum + int64 index) * int64 c,
+            index + 1)
+        |> fst  // Discard the index
+        |> should equal 1464868296444951516L
 
     [<TestCase>]
     let foldBack () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        (String.empty, (0L, 0))
+        ||> String.foldBack (fun c (checksum, index) ->
+            (checksum + int64 index) * int64 c,
+            index + 1)
+        |> fst  // Discard the index
+        |> should equal 0L
+
+        // Test case for "normal" usage of this function.
+        ("The quick brown fox jumps over the lazy dog.", (0L, 0))
+        ||> String.foldBack (fun c (checksum, index) ->
+            (checksum + int64 index) * int64 c,
+            index + 1)
+        |> fst  // Discard the index
+        |> should equal 6529802340200151804L
 
     [<TestCase>]
     let iter () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty substring.
+        do
+            let elements = ResizeArray ()
+
+            String.empty
+            |> String.iter (System.Char.ToUpper >> elements.Add)
+
+            elements.ToArray ()
+            |> Array.isEmpty
+            |> should be True
+
+        // Test case for "normal" usage of this function.
+        do
+            let elements = ResizeArray ()
+
+            "The quick brown fox jumps over the lazy dog."
+            |> String.iter (System.Char.ToUpper >> elements.Add)
+
+            elements.ToArray ()
+            |> should equal
+                [| 'T'; 'H'; 'E'; ' '; 'Q'; 'U'; 'I'; 'C'; 'K'; ' '; 'B'; 'R'; 'O'; 'W'; 'N';
+                   ' '; 'F'; 'O'; 'X'; ' '; 'J'; 'U'; 'M'; 'P'; 'S'; ' '; 'O'; 'V'; 'E'; 'R';
+                   ' '; 'T'; 'H'; 'E'; ' '; 'L'; 'A'; 'Z'; 'Y'; ' '; 'D'; 'O'; 'G'; '.'; |]
 
     [<TestCase>]
     let iteri () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty substring.
+        do
+            let elements = ResizeArray ()
+
+            String.empty
+            |> String.iteri (fun idx c ->
+                elements.Add (
+                    if idx % 2 = 0 then System.Char.ToUpper c else c))
+
+            elements.ToArray ()
+            |> Array.isEmpty
+            |> should be True
+
+        // Test case for "normal" usage of this function.
+        do
+            let elements = ResizeArray ()
+
+            "The quick brown fox jumps over the lazy dog."
+            |> String.iteri (fun idx c ->
+                elements.Add (
+                    if idx % 2 = 0 then System.Char.ToUpper c else c))
+
+            elements.ToArray ()
+            |> should equal
+                [| 'T'; 'h'; 'E'; ' '; 'Q'; 'u'; 'I'; 'c'; 'K'; ' '; 'B'; 'r'; 'O'; 'w'; 'N';
+                   ' '; 'F'; 'o'; 'X'; ' '; 'J'; 'u'; 'M'; 'p'; 'S'; ' '; 'O'; 'v'; 'E'; 'r';
+                   ' '; 't'; 'H'; 'e'; ' '; 'l'; 'A'; 'z'; 'Y'; ' '; 'D'; 'o'; 'G'; '.'; |]
 
     [<TestCase>]
     let map () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.map System.Char.ToUpper
+        |> should equal String.empty
+
+        // Test case for "normal" usage of this function.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.map System.Char.ToUpper
+        |> should equal "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
 
     [<TestCase>]
     let mapi () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.mapi (fun idx c ->
+            if idx % 2 = 0 then System.Char.ToUpper c else c)
+        |> should equal String.empty
+
+        // Test case for "normal" usage of this function.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.mapi (fun idx c ->
+            if idx % 2 = 0 then System.Char.ToUpper c else c)
+        |> should equal "ThE QuIcK BrOwN FoX JuMpS OvEr tHe lAzY DoG."
 
     [<TestCase>]
     let choose () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.choose (fun c ->
+            if int c % 2 = 0 then
+                Some (System.Char.ToUpper c)
+            else None)
+        |> should equal String.empty
+
+        // Test case for "normal" usage of this function.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.choose (fun c ->
+            if int c % 2 = 0 then
+                Some (System.Char.ToUpper c)
+            else None)
+        |> should equal "TH  BRN FX JP VR TH LZ D."
 
     [<TestCase>]
     let choosei () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.choosei (fun idx c ->
+            if (idx + int c) % 2 = 0 then
+                Some (System.Char.ToUpper c)
+            else None)
+        |> should equal String.empty
+
+        // Test case for "normal" usage of this function.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.choosei (fun idx c ->
+            if (idx + int c) % 2 = 0 then
+                Some (System.Char.ToUpper c)
+            else None)
+        |> should equal "TUCBWNFOXJU HE DO"
+
+    [<TestCase>]
+    let trimChars () : unit =
+        // Test case for empty string.
+        String.empty
+        |> String.trim [| ' '; '.'; ','; |]
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for a string which DOES NOT start or end with any of the specified characters.
+        "Hello World!"
+        |> String.trim [| ' '; '.'; ','; |]
+        |> should equal "Hello World!"
+
+        // Test case for a string which DOES start AND end with one or more of the specified characters.
+        "    ,, ,Hello World!"
+        |> String.trim [| ' '; '.'; ','; |]
+        |> should equal "Hello World!"
 
     [<TestCase>]
     let trimStart () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.trimStart [| ' '; '.'; ','; |]
+        |> should equal String.empty
+
+        // Test case for a string which DOES NOT start with any of the specified characters.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.trimStart [| ' '; '.'; ','; |]
+        |> should equal "The quick brown fox jumps over the lazy dog."
+
+        // Test case for a string which DOES start with one or more of the specified characters.
+        "    ,, ,Hello World!"
+        |> String.trimStart [| ' '; '.'; ','; |]
+        |> should equal "Hello World!"
 
     [<TestCase>]
     let trimEnd () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.trimEnd [| ' '; '.'; ','; |]
+        |> should equal String.empty
+
+        // Test case for when the string DOES NOT end with any of the specified characters.
+        "The quick brown fox jumps over the lazy dog."
+        |> String.trimEnd [| '!'; '?'; '*'; |]
+        |> should equal "The quick brown fox jumps over the lazy dog."
+
+        // Test case for when the string DOES end with one or more of the specified characters.
+        "Hello World!1!?!!"
+        |> String.trimEnd [| '!'; '?'; '*'; |]
+        |> should equal "Hello World!1"
 
     [<TestCase>]
     let trimStartWith () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.trimStartWith (not << System.Char.IsWhiteSpace)
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for non-empty string with leading characters which
+        // don't match the predicate (i.e., they'll be filtered out of the string).
+        "    ,, ,Hello World!"
+        |> String.trimStartWith (not << System.Char.IsWhiteSpace)
+        |> should equal ",, ,Hello World!"
 
     [<TestCase>]
     let trimEndWith () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.trimEndWith (not << System.Char.IsPunctuation)
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for non-empty string with trailing characters which
+        // don't match the predicate (i.e., they'll be filtered out of the string).
+        "Hello World!1!?!!"
+        |> String.trimEndWith (not << System.Char.IsPunctuation)
+        |> should equal "Hello World!1"
 
     [<TestCase>]
     let trimWith () : unit =
-        Assert.Inconclusive "Test not yet implemented."
+        // Test case for empty string.
+        String.empty
+        |> String.trimWith System.Char.IsLetterOrDigit
+        |> String.isEmpty
+        |> should be True
+
+        // Test case for non-empty string with leading AND trailing characters which
+        // don't match the predicate (i.e., they'll be filtered out of the string).
+        "    ,, ,Hello World!1!?!!"
+        |> String.trimWith System.Char.IsLetterOrDigit
+        |> should equal "Hello World!1"
 
     module Split =
         [<TestCase>]
