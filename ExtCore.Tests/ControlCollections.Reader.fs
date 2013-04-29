5d5a631412bdc96c1edadc8691e0fac1aaff5247
Jack Pappas <jackp@server.fake>
2013-04-28 20:29:02 -0400

    Implemented tests for Reader.Array functions.

--- ExtCore.Tests/ControlCollections.Reader.fs
+++ ExtCore.Tests/ControlCollections.Reader.fs
@@ -23,24 +23,143 @@ open ExtCore.Control
 open ExtCore.Control.Collections
 open NUnit.Framework
 open FsUnit
-//open FsCheck
+
+
+/// Helper functions for implementing tests.
+[<AutoOpen>]
+module private ReaderTestHelpers =
+    /// A subset of the F# language keywords.
+    let keywords = Set.ofArray [| "if"; "then"; "elif"; "else"; "do"; "while"; "let"; "for"; |]
 
 
 /// Tests for the ExtCore.Control.Collections.Reader.Array module.
 module Array =
     [<TestCase>]
     let iter () : unit =
-        Assert.Ignore "Test not yet implemented."
+        // Test case for an empty array.
+        do
+            let keywordCount = ref 0
+
+            let testFunc =
+                Array.empty
+                |> Reader.Array.iter (fun str ->
+                    reader {
+                    let! isKeyword = Set.contains str
+                    if isKeyword then
+                        incr keywordCount
+                    })
+            Reader.run testFunc keywords
+
+            !keywordCount |> should equal 0
+
+        // Sample usage test cases.
+        do
+            let keywordCount = ref 0
+
+            let testFunc =
+                [| "if"; "foo"; "<>"; "123"; "then"; |]
+                |> Reader.Array.iter (fun str ->
+                    reader {
+                    let! isKeyword = Set.contains str
+                    if isKeyword then
+                        incr keywordCount
+                    })
+            Reader.run testFunc keywords
+
+            !keywordCount |> should equal 2
 
     [<TestCase>]
     let iteri () : unit =
-        Assert.Ignore "Test not yet implemented."
+        // Test case for an empty array.
+        do
+            let keywordWeight = ref 0
+
+            let testFunc =
+                Array.empty
+                |> Reader.Array.iteri (fun idx str ->
+                    reader {
+                    let! isKeyword = Set.contains str
+                    if isKeyword then
+                        keywordWeight := !keywordWeight + (idx + 1)
+                    })
+            Reader.run testFunc keywords
+
+            !keywordWeight |> should equal 0
+
+        // Sample usage test cases.
+        do
+            let keywordWeight = ref 0
+
+            let testFunc =
+                [| "if"; "foo"; "<>"; "123"; "then"; |]
+                |> Reader.Array.iteri (fun idx str ->
+                    reader {
+                    let! isKeyword = Set.contains str
+                    if isKeyword then
+                        keywordWeight := !keywordWeight + (idx + 1)
+                    })
+            Reader.run testFunc keywords
+
+            !keywordWeight |> should equal 6
 
     [<TestCase>]
     let map () : unit =
-        Assert.Ignore "Test not yet implemented."
+        // Test case for an empty array.
+        do
+            let testFunc =
+                Array.empty
+                |> Reader.Array.map (fun str ->
+                    reader {
+                    return! Set.contains str
+                    })
+
+            keywords
+            |> Reader.run testFunc
+            |> Array.isEmpty
+            |> should be True
+
+        // Sample usage test cases.
+        do
+            let testFunc =
+                [| "if"; "foo"; "<>"; "123"; "then"; |]
+                |> Reader.Array.map (fun str ->
+                    reader {
+                    return! Set.contains str
+                    })
+            keywords
+            |> Reader.run testFunc
+            |> should equal
+                [| true; false; false; false; true; |]
 
     [<TestCase>]
     let mapi () : unit =
-        Assert.Ignore "Test not yet implemented."
+        // Test case for an empty array.
+        do
+            let testFunc =
+                Array.empty
+                |> Reader.Array.mapi (fun idx str ->
+                    reader {
+                    let! isKeyword = Set.contains str
+                    return
+                        if isKeyword then Some (sprintf "%i:%s" idx str) else None
+                    })
+            keywords
+            |> Reader.run testFunc
+            |> Array.isEmpty
+            |> should be True
+
+        // Sample usage test cases.
+        do
+            let testFunc =
+                [| "if"; "foo"; "<>"; "123"; "then"; |]
+                |> Reader.Array.mapi (fun idx str ->
+                    reader {
+                    let! isKeyword = Set.contains str
+                    return
+                        if isKeyword then Some (sprintf "%i:%s" idx str) else None
+                    })
+            keywords
+            |> Reader.run testFunc
+            |> should equal
+                [| Some "0:if"; None; None; None; Some "4:then"; |]
 
