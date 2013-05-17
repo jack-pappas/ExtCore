4bbe8a7b67f5b64a40c4141e2c754a458234777f
Jack Pappas <jackp@server.fake>
2013-05-17 16:02:22 -0400

    Incremented version number *twice*, to 0.8.20 (due to a botched release of 0.8.18).

--- ExtCore/AssemblyInfo.fs
+++ ExtCore/AssemblyInfo.fs
@@ -29,9 +29,9 @@ open System.Security.Permissions
 
 
 // Version information
-[<assembly: AssemblyVersion("0.8.18")>]
-[<assembly: AssemblyFileVersion("0.8.18")>]
-[<assembly: AssemblyInformationalVersion("0.8.18")>]
+[<assembly: AssemblyVersion("0.8.20")>]
+[<assembly: AssemblyFileVersion("0.8.20")>]
+[<assembly: AssemblyInformationalVersion("0.8.20")>]
 
 // Assembly information
 [<assembly: AssemblyTitle("ExtCore")>]
