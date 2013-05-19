a41be53302367562ef74ce574fd1d9058759acb1
Jack Pappas <jackp@server.fake>
2013-05-19 08:41:24 -0400

    Created a file which'll hold the tests for HashSet.

---
+++ ExtCore.Tests/Collections.HashSet.fs
@@ -0,0 +1,31 @@
+﻿(*
+
+Copyright 2005-2009 Microsoft Corporation
+Copyright 2013 Jack Pappas
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+/// Unit tests for the ExtCore.Collections.HashSet type and module.
+module Tests.ExtCore.Collections.HashSet
+
+open System
+open System.Collections
+open System.Collections.Generic
+open NUnit.Framework
+open FsUnit
+//open FsCheck
+
+(* TODO : Implement tests for HashSet. *)
+
