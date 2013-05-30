3b1fa3bdc944a607d75da41e7a19c7c25ead1391
Jack Pappas <jackp@server.fake>
2013-05-30 11:42:33 -0400

    Implemented most of the remaining functions in the LruCache module. Modified the LruCache functions which create/populate an LruCache from a source so they accept a capacity value.

--- ExtCore/Caching.LruCache.fs
+++ ExtCore/Caching.LruCache.fs
@@ -255,38 +255,38 @@ type LruCache<'Key, 'T when 'Key : comparison>
             Map.add key kvp.Value map)
 
     //
-    static member internal OfSeq (source : seq<'Key * 'T>) : LruCache<'Key, 'T> =
+    static member internal OfSeq (source : seq<'Key * 'T>, capacity : uint32) : LruCache<'Key, 'T> =
         // Preconditions
         checkNonNull "source" source
 
-        (empty, source)
+        (LruCache.Create capacity, source)
         ||> Seq.fold (fun cache (key, value) ->
             cache.Add (key, value))
 
     //
-    static member internal OfList (source : ('Key * 'T) list) : LruCache<'Key, 'T> =
+    static member internal OfList (source : ('Key * 'T) list, capacity : uint32) : LruCache<'Key, 'T> =
         // Preconditions
         checkNonNull "source" source
 
-        (empty, source)
+        (LruCache.Create capacity, source)
         ||> List.fold (fun cache (key, value) ->
             cache.Add (key, value))
 
     //
-    static member internal OfArray (source : ('Key * 'T)[]) : LruCache<'Key, 'T> =
+    static member internal OfArray (source : ('Key * 'T)[], capacity : uint32) : LruCache<'Key, 'T> =
         // Preconditions
         checkNonNull "source" source
         
-        (empty, source)
+        (LruCache.Create capacity, source)
         ||> Array.fold (fun cache (key, value) ->
             cache.Add (key, value))
 
     //
-    static member internal OfMap (source : Map<'Key, 'T>) : LruCache<'Key, 'T> =
+    static member internal OfMap (source : Map<'Key, 'T>, capacity : uint32) : LruCache<'Key, 'T> =
         // Preconditions
         checkNonNull "source" source
 
-        (empty, source)
+        (LruCache.Create capacity, source)
         ||> Map.fold (fun cache key value ->
             cache.Add (key, value))
 
@@ -294,6 +294,200 @@ type LruCache<'Key, 'T when 'Key : comparison>
     static member internal Create (capacity : uint32) : LruCache<'Key, 'T> =
         LruCache (HashMap.empty, IntMap.empty, capacity, 0u)
 
+    //
+    member __.TryPick (picker : 'Key -> 'T -> 'U option) : 'U option =
+        // Adapt the picker for better performance.
+        let picker = FSharpFunc<_,_,_>.Adapt picker
+
+        // Traverse the elements in the cache from oldest to newest.
+        indexedKeys
+        |> IntMap.tryPick (fun _ key ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the picker to the key and value.
+            picker.Invoke (key, kvp.Value))
+
+    //
+    member this.Pick (picker : 'Key -> 'T -> 'U option) : 'U =
+        // Call TryPick, and raise an exception if no value is picked.
+        match this.TryPick picker with
+        | Some x -> x
+        | None ->
+            // TODO : Provide a better error message.
+            // keyNotFound ""
+            raise <| System.Collections.Generic.KeyNotFoundException ()
+
+    //
+    member __.Exists (predicate : 'Key -> 'T -> bool) : bool =
+        // Adapt the predicate for better performance.
+        let predicate = FSharpFunc<_,_,_>.Adapt predicate
+
+        // Traverse the elements in the cache from oldest to newest.
+        indexedKeys
+        |> IntMap.exists (fun _ key ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the predicate to the key and value.
+            predicate.Invoke (key, kvp.Value))
+
+    //
+    member __.Forall (predicate : 'Key -> 'T -> bool) : bool =
+        // Adapt the predicate for better performance.
+        let predicate = FSharpFunc<_,_,_>.Adapt predicate
+
+        // Traverse the elements in the cache from oldest to newest.
+        indexedKeys
+        |> IntMap.forall (fun _ key ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the predicate to the key and value.
+            predicate.Invoke (key, kvp.Value))
+
+    //
+    member this.Filter (predicate : 'Key -> 'T -> bool) : LruCache<'Key, 'T> =
+        // Adapt the predicate for better performance.
+        let predicate = FSharpFunc<_,_,_>.Adapt predicate
+
+        // Remove the keys which don't match the predicate.
+        (this, indexedKeys)
+        ||> IntMap.fold (fun this _ key ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the predicate to the key and value;
+            // if it doesn't match, remove the key.
+            if predicate.Invoke (key, kvp.Value) then this
+            else this.Remove key)
+
+    //
+    member this.Choose (chooser : 'Key -> 'T -> 'U option) : LruCache<'Key, 'U> =
+        // Adapt the chooser for better performance.
+        let chooser = FSharpFunc<_,_,_>.Adapt chooser
+
+        // Remove the keys which aren't chosen, and create a new HashMap
+        // to hold the chosen values.
+        let cache, indexedKeys =
+            ((HashMap.empty, indexedKeys), indexedKeys)
+            ||> IntMap.fold (fun (chosenCache, indexedKeys) _ key ->
+                // Get the value from the cache.
+                let kvp = HashMap.find key cache
+
+                // Apply the chooser to the key and value;
+                // if it doesn't match, remove the key.
+                match chooser.Invoke (key, kvp.Value) with
+                | None ->
+                    let indexedKeys' = IntMap.remove kvp.Key indexedKeys
+                    chosenCache, indexedKeys'
+                | Some chosenValue ->
+                    let chosenCache' =
+                        HashMap.add key (KeyValuePair (kvp.Key, chosenValue)) chosenCache
+                    chosenCache', indexedKeys)
+
+        // Return a new cache with the updated maps.
+        // OPTIMIZE : Only return a new cache if the maps were actually updated.
+        LruCache (cache, indexedKeys, capacity, currentIndex)
+
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// oldest (least-recently-used) to newest (most-recently-used).
+    member this.Iterate (action : 'Key -> 'T -> unit) : unit =
+        // Adapt the action for better performance.
+        let action = FSharpFunc<_,_,_>.Adapt action
+
+        // Iterate over the keys.
+        indexedKeys
+        |> IntMap.iter (fun _ key ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the action to the key and value.
+            action.Invoke (key, kvp.Value))
+
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// newest (most-recently-used) to oldest (least-recently-used).
+    member this.IterateBack (action : 'Key -> 'T -> unit) : unit =
+        // Adapt the action for better performance.
+        let action = FSharpFunc<_,_,_>.Adapt action
+
+        // Iterate over the keys.
+        indexedKeys
+        |> IntMap.iterBack (fun _ key ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the action to the key and value.
+            action.Invoke (key, kvp.Value))
+
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// oldest (least-recently-used) to newest (most-recently-used), threading an
+    /// accumulator value through the computation.
+    member this.Fold (folder : 'State -> 'Key -> 'T -> 'State, state : 'State) : 'State =
+        // Adapt the folder for better performance.
+        let folder = FSharpFunc<_,_,_,_>.Adapt folder
+
+        // Fold over the keys.
+        (state, indexedKeys)
+        ||> IntMap.fold (fun state _ key ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the folder to the key and value and the current state value.
+            folder.Invoke (state, key, kvp.Value))
+
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// newest (most-recently-used) to oldest (least-recently-used), threading an
+    /// accumulator value through the computation.
+    member this.FoldBack (folder : 'Key -> 'T -> 'State -> 'State, state : 'State) : 'State =
+        // Adapt the folder for better performance.
+        let folder = FSharpFunc<_,_,_,_>.Adapt folder
+
+        // Fold over the keys.
+        (indexedKeys, state)
+        ||> IntMap.foldBack (fun _ key state ->
+            // Get the value from the cache.
+            let kvp = HashMap.find key cache
+
+            // Apply the folder to the key and value and the current state value.
+            folder.Invoke (key, kvp.Value, state))
+
+    /// Builds a new cache whose values are the results of applying the given function
+    /// to each key-value pair in the cache. The key passed to the function indicates
+    /// the key of the element being transformed.
+    member this.Map (mapping : 'Key -> 'T -> 'U) : LruCache<'Key, 'U> =
+        // Adapt the mapping for better performance.
+        let mapping = FSharpFunc<_,_,_>.Adapt mapping
+
+        // We fold over the cache-map (HashMap) here instead of the key-index (IntMap)
+        // because it is faster than having to look up the value corresponding to each key.
+        // However, this also means the mapping is not applied to the elements in
+        // any particular order (oldest-to-newest or newest-to-oldest).
+        let mappedCache =
+            cache
+            |> HashMap.map (fun key (KeyValue (keyIndex, value)) ->
+                // Apply the mapping to the key and value.
+                let mappedValue = mapping.Invoke (key, value)
+
+                // Return a new KeyValuePair with the key index and mapped value.
+                KeyValuePair (keyIndex, mappedValue))
+
+        // Return a new cache with the mapped cache-map.
+        LruCache (mappedCache, indexedKeys, capacity, currentIndex)
+
+    //
+    member this.Partition (predicate : 'Key -> 'T -> bool) : LruCache<'Key, 'T> * LruCache<'Key, 'T> =
+        // Adapt the predicate for better performance.
+        let predicate = FSharpFunc<_,_,_>.Adapt predicate
+
+        notImpl "LruCache.Partition"
+
+    //
+    member this.MapPartition (partitioner : 'Key -> 'T -> Choice<'U, 'V>) : LruCache<'Key, 'U> * LruCache<'Key, 'V> =
+        // Adapt the partitioner for better performance.
+        let partitioner = FSharpFunc<_,_,_>.Adapt partitioner
+
+        notImpl "LruCache.MapPartition"
 
 //
 [<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
@@ -381,27 +575,27 @@ module LruCache =
 
     //
     [<CompiledName("OfSeq")>]
-    let ofSeq (source : seq<'Key * 'T>) : LruCache<'Key, 'T> =
+    let ofSeq (capacity : uint32) (source : seq<'Key * 'T>) : LruCache<'Key, 'T> =
         // Preconditions checked within the member.
-        LruCache.OfSeq source
+        LruCache.OfSeq (source, capacity)
 
     //
     [<CompiledName("OfList")>]
-    let ofList (source : ('Key * 'T) list) : LruCache<'Key, 'T> =
+    let ofList (capacity : uint32) (source : ('Key * 'T) list) : LruCache<'Key, 'T> =
         // Preconditions checked within the member.
-        LruCache.OfList source
+        LruCache.OfList (source, capacity)
 
     //
     [<CompiledName("OfArray")>]
-    let ofArray (source : ('Key * 'T)[]) : LruCache<'Key, 'T> =
+    let ofArray (capacity : uint32) (source : ('Key * 'T)[]) : LruCache<'Key, 'T> =
         // Preconditions checked within the member.
-        LruCache.OfArray source
+        LruCache.OfArray (source, capacity)
 
     //
     [<CompiledName("OfMap")>]
-    let ofMap (source : Map<'Key, 'T>) : LruCache<'Key, 'T> =
+    let ofMap (capacity : uint32) (source : Map<'Key, 'T>) : LruCache<'Key, 'T> =
         // Preconditions checked within the member.
-        LruCache.OfMap source
+        LruCache.OfMap (source, capacity)
 
     //
     [<CompiledName("ToSeq")>]
@@ -434,34 +628,40 @@ module LruCache =
         checkNonNull "cache" cache
 
         cache.ToMap ()
-(*
-    /// Searches the map looking for the first element where the given function
+
+    /// Searches the cache looking for the least-recently-used element where the given function
     /// returns a Some value. If no such element is found, returns None.
     [<CompiledName("TryPick")>]
     let inline tryPick (picker : 'Key -> 'T -> 'U option) (cache : LruCache<'Key, 'T>) : 'U option =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.TryPick picker
+        cache.TryPick picker
 
-    /// Searches the map looking for the first element where the given function
-    /// returns a Some value.
+    /// Searches the cache looking for the least-recently-used element where the given function
+    /// returns a Some value. If no such element is found, KeyNotFoundException is raised.
     [<CompiledName("Pick")>]
     let inline pick (picker : 'Key -> 'T -> 'U option) (cache : LruCache<'Key, 'T>) : 'U =
         // Preconditions
         checkNonNull "cache" cache
 
-        map.Pick picker
+        cache.Pick picker
 
-    /// Builds a new collection whose elements are the results of applying the given function
-    /// to each of the elements of the collection. The key passed to the function indicates
-    /// the key of the element being transformed.
-    [<CompiledName("Map")>]
-    let inline map (mapping : 'Key -> 'T -> 'U) (cache : LruCache<'Key, 'T>) : LruCache<'Key, 'U> =
+    /// Determines if any binding in the map matches the specified predicate.
+    [<CompiledName("Exists")>]
+    let inline exists (predicate : 'Key -> 'T -> bool) (cache : LruCache<'Key, 'T>) : bool =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.Map mapping
+        cache.Exists predicate
+
+    /// Determines if all bindings in the map match the specified predicate.
+    [<CompiledName("Forall")>]
+    let inline forall (predicate : 'Key -> 'T -> bool) (cache : LruCache<'Key, 'T>) : bool =
+        // Preconditions
+        checkNonNull "cache" cache
+
+        cache.Forall predicate
 
     /// <summary>
     /// Builds a new map containing only the bindings for which the given
@@ -472,7 +672,7 @@ module LruCache =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.Filter predicate
+        cache.Filter predicate
 
     /// <summary>
     /// Applies the given function to each binding in the map.
@@ -484,71 +684,71 @@ module LruCache =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.Choose chooser
+        cache.Choose chooser
 
-    /// Applies the given function to each binding in the map.
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// oldest (least-recently-used) to newest (most-recently-used).
     [<CompiledName("Iterate")>]
     let inline iter (action : 'Key -> 'T -> unit) (cache : LruCache<'Key, 'T>) : unit =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.Iterate action
+        cache.Iterate action
 
-    /// Applies the given function to each binding in the map.
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// newest (most-recently-used) to oldest (least-recently-used).
     [<CompiledName("IterateBack")>]
     let inline iterBack (action : 'Key -> 'T -> unit) (cache : LruCache<'Key, 'T>) : unit =
         // Preconditions
         checkNonNull "cache" cache
 
-        map.IterateBack action
+        cache.IterateBack action
 
-    /// Folds over the bindings in the map.
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// oldest (least-recently-used) to newest (most-recently-used), threading an
+    /// accumulator value through the computation.
     [<CompiledName("Fold")>]
     let inline fold (folder : 'State -> 'Key -> 'T -> 'State) (state : 'State) (cache : LruCache<'Key, 'T>) : 'State =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.Fold (folder, state)
+        cache.Fold (folder, state)
 
-    /// Folds over the bindings in the map.
+    /// Apply a function to each key-value pair in the cache, in order from
+    /// newest (most-recently-used) to oldest (least-recently-used), threading an
+    /// accumulator value through the computation.
     [<CompiledName("FoldBack")>]
     let inline foldBack (folder : 'Key -> 'T -> 'State -> 'State) (cache : LruCache<'Key, 'T>) (state : 'State) : 'State =
         // Preconditions
         checkNonNull "cache" cache
 
-        map.FoldBack (folder, state)
+        cache.FoldBack (folder, state)
 
-    /// Determines if any binding in the map matches the specified predicate.
-    [<CompiledName("Exists")>]
-    let inline exists (predicate : 'Key -> 'T -> bool) (cache : LruCache<'Key, 'T>) : bool =
+    /// Builds a new cache whose values are the results of applying the given function
+    /// to each key-value pair in the cache. The key passed to the function indicates
+    /// the key of the element being transformed.
+    [<CompiledName("Map")>]
+    let inline map (mapping : 'Key -> 'T -> 'U) (cache : LruCache<'Key, 'T>) : LruCache<'Key, 'U> =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.Exists predicate
+        cache.Map mapping
 
-    /// Determines if all bindings in the map match the specified predicate.
-    [<CompiledName("Forall")>]
-    let inline forall (predicate : 'Key -> 'T -> bool) (cache : LruCache<'Key, 'T>) : bool =
-        // Preconditions
-        checkNonNull "cache" cache
-
-        map.Forall predicate
-
-    /// Splits the map into two maps containing the bindings for which the given
+    /// Splits the cache into two caches containing the bindings for which the given
     /// predicate returns true and false, respectively.
     [<CompiledName("Partition")>]
     let inline partition (predicate : 'Key -> 'T -> bool) (cache : LruCache<'Key, 'T>) : LruCache<'Key, 'T> * LruCache<'Key, 'T> =
         // Preconditions
         checkNonNull "cache" cache
         
-        map.Partition predicate
+        cache.Partition predicate
 
-    /// Splits the map into two maps by applying the given partitioning function
-    /// to each binding in the map.
+    /// Splits the cache into two caches by applying the given partitioning function
+    /// to each binding in the cache.
     [<CompiledName("MapPartition")>]
     let inline mapPartition (partitioner : 'Key -> 'T -> Choice<'U, 'V>) (cache : LruCache<'Key, 'T>) : LruCache<'Key, 'U> * LruCache<'Key, 'V> =
         // Preconditions
         checkNonNull "cache" cache
 
-        map.MapPartition partitioner
-*)
+        cache.MapPartition partitioner
+
