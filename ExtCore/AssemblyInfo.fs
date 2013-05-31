12faafb8e2ea57ff2da1ffd6054fc650eeb158d0
Jack Pappas <jackp@server.fake>
2013-05-31 12:55:09 -0400

    Incremented version number to 0.8.24.

--- ExtCore/AssemblyInfo.fs
+++ ExtCore/AssemblyInfo.fs
@@ -29,9 +29,9 @@ open System.Security.Permissions
 
 
 // Version information
-[<assembly: AssemblyVersion("0.8.23")>]
-[<assembly: AssemblyFileVersion("0.8.23")>]
-[<assembly: AssemblyInformationalVersion("0.8.23")>]
+[<assembly: AssemblyVersion("0.8.24")>]
+[<assembly: AssemblyFileVersion("0.8.24")>]
+[<assembly: AssemblyInformationalVersion("0.8.24")>]
 
 // Assembly information
 [<assembly: AssemblyTitle("ExtCore")>]
