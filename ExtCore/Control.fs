faaa092eb65e6c9908f717c060f5168066d46e88
Jack Pappas <jackp@server.fake>
2013-11-09 17:21:32 -0500

    Reformatting to adhere to ExtCore coding style.

--- ExtCore/Control.fs
+++ ExtCore/Control.fs
@@ -31,30 +31,38 @@ open ExtCore
 module AsyncExtensions =
     open ExtCore.Control.Agents
 
-    type Microsoft.FSharp.Control.Async with 
-
+    type Microsoft.FSharp.Control.Async with
         /// Creates an asynchronous workflow that runs the asynchronous workflow
         /// given as an argument at most once. When the returned workflow is 
         /// started for the second time, it reuses the result of the 
         /// previous execution.
-        static member Cache (input:Async<'T>) = 
-            let agent = Agent<AsyncReplyChannel<_>>.Start(fun agent -> async {
-                let! repl = agent.Receive()
+        static member Cache (input : Async<'T>) =
+            // Preconditions
+            checkNonNull "input" input
+
+            let agent = Agent<AsyncReplyChannel<_>>.Start <| fun agent ->
+                async {
+                let! repl = agent.Receive ()
                 let! res = input
-                repl.Reply(res)
+                repl.Reply res
                 while true do
-                    let! repl = agent.Receive()
-                    repl.Reply(res) })
-            async { return! agent.PostAndAsyncReply(id) }
+                    let! repl = agent.Receive ()
+                    repl.Reply res }
+
+            async { return! agent.PostAndAsyncReply id }
 
         /// Starts the specified operation using a new CancellationToken and returns
         /// IDisposable object that cancels the computation. This method can be used
         /// when implementing the Subscribe method of IObservable interface.
-        static member StartDisposable(op:Async<unit>) =
-            let ct = new System.Threading.CancellationTokenSource()
-            Async.Start(op, ct.Token)
-            { new System.IDisposable with 
-                member x.Dispose() = ct.Cancel() }
+        static member StartDisposable (op : Async<unit>) =
+            // Preconditions
+            checkNonNull "op" op
+
+            let ct = new System.Threading.CancellationTokenSource ()
+            Async.Start (op, ct.Token)
+            { new System.IDisposable with
+                member x.Dispose () =
+                    ct.Cancel () }
 
 
 (*** Workflow Monoids ***)
--- ExtCore/Net.fs
+++ ExtCore/Net.fs
@@ -30,51 +30,64 @@ open ExtCore.IO
 
 /// Extensions that simplify working with HttpListener and related types.
 [<AutoOpen>]
-module HttpExtensions = 
+module HttpExtensions =
+    type System.Net.HttpListener with
+        /// Asynchronously waits for an incoming request and returns it.
+        member this.AsyncGetContext () =
+            Async.FromBeginEnd (this.BeginGetContext, this.EndGetContext)
 
-  type System.Net.HttpListener with
-    /// Asynchronously waits for an incoming request and returns it.
-    member x.AsyncGetContext() = 
-      Async.FromBeginEnd(x.BeginGetContext, x.EndGetContext)
+        /// Starts HttpListener on the specified URL. The 'handler' function is
+        /// called (in a new thread pool thread) each time an HTTP request is received.
+        static member Start (url, handler, ?cancellationToken) =
+            let server =
+                async {
+                use listener = new HttpListener ()
+                listener.Prefixes.Add url
+                listener.Start ()
+                while true do
+                  let! context = listener.AsyncGetContext ()
+                  let asyncHandler = handler (context.Request, context.Response)
+                  Async.Start (asyncHandler, ?cancellationToken = cancellationToken)
+                }
 
-    /// Starts HttpListener on the specified URL. The 'handler' function is
-    /// called (in a new thread pool thread) each time an HTTP request is received.
-    static member Start(url, handler, ?cancellationToken) =
-      let server = async { 
-        use listener = new HttpListener()
-        listener.Prefixes.Add(url)
-        listener.Start()
-        while true do 
-          let! context = listener.AsyncGetContext()
-          Async.Start
-            ( handler (context.Request, context.Response), 
-              ?cancellationToken = cancellationToken) }
-      Async.Start(server, ?cancellationToken = cancellationToken)
+            Async.Start (server, ?cancellationToken = cancellationToken)
 
-  type System.Net.HttpListenerRequest with
-    /// Asynchronously reads the 'InputStream' of the request and converts it to a string
-    member request.AsyncInputString = async {
-      use tmp = new MemoryStream()
-      for data in request.InputStream.AsyncReadSeq(16 * 1024) do
-        tmp.Write(data, 0, data.Length) 
-      tmp.Seek(0L, SeekOrigin.Begin) |> ignore
-      use sr = new StreamReader(tmp)
-      return sr.ReadToEnd() }
+    type System.Net.HttpListenerRequest with
+        /// Asynchronously reads the 'InputStream' of the request and converts it to a string
+        member this.AsyncInputString =
+            async {
+            use tmp = new MemoryStream ()
+            for data in this.InputStream.AsyncReadSeq (16 * 1024) do
+                tmp.Write (data, 0, data.Length)
+            tmp.Seek (0L, SeekOrigin.Begin) |> ignore
+            use sr = new StreamReader (tmp)
+            return sr.ReadToEnd ()
+            }
 
+    type System.Net.HttpListenerResponse with
+        /// Sends the specified string as a reply in UTF-8 encoding
+        member this.AsyncReply (str : string) =
+            // Preconditions
+            checkNonNull "str" str
 
-  type System.Net.HttpListenerResponse with
-    /// Sends the specified string as a reply in UTF 8 encoding
-    member response.AsyncReply(s:string) = async {
-      let buffer = Encoding.UTF8.GetBytes(s)
-      response.ContentLength64 <- int64 buffer.Length
-      let output = response.OutputStream
-      do! output.AsyncWrite(buffer,0,buffer.Length)
-      output.Close() }
+            async {
+            let buffer = Encoding.UTF8.GetBytes str
+            this.ContentLength64 <- int64 buffer.Length
+            let output = this.OutputStream
+            do! output.AsyncWrite (buffer, 0, buffer.Length)
+            output.Close ()
+            }
 
-    /// Sends the specified data as a reply with the specified content type
-    member response.AsyncReply(typ, buffer:byte[]) = async {
-      response.ContentLength64 <- int64 buffer.Length
-      let output = response.OutputStream
-      response.ContentType <- typ
-      do! output.AsyncWrite(buffer,0,buffer.Length)
-      output.Close() }
+        /// Sends the specified data as a reply with the specified content type
+        member this.AsyncReply (contentType, buffer : byte[]) =
+            // Preconditions
+            //checkNonNull "contentType" contentType
+            checkNonNull "buffer" buffer
+
+            async {
+            this.ContentLength64 <- int64 buffer.Length
+            let output = this.OutputStream
+            this.ContentType <- contentType
+            do! output.AsyncWrite (buffer, 0, buffer.Length)
+            output.Close ()
+            }
