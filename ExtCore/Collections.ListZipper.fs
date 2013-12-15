9246906f51241fbe0348f758ab8275421f9e3d2e
Jack Pappas <jackp@server.fake>
2013-12-14 20:30:01 -0500

    Added full documentation comments for ListZipper.

--- ExtCore/Collections.ListZipper.fs
+++ ExtCore/Collections.ListZipper.fs
@@ -21,39 +21,39 @@ namespace ExtCore.Collections
 open ExtCore
 
 
-/// An implementation of a 'zipper' for F# lists.
+/// <summary>An implementation of a 'zipper' for F# lists.</summary>
+/// <typeparam name="T"></typeparam>
 [<Struct; NoComparison>]
 type ListZipper<'T> private (path : 'T list, list : 'T list) =
-    /// The empty ListZipper value.
+    /// <summary>The empty <see cref="ListZipper`1"/> value.</summary>
     static let empty : ListZipper<'T> =
         ListZipper ([], [])
 
-    /// The empty ListZipper value.
+    /// <summary>The empty <see cref="ListZipper`1"/> value.</summary>
     static member Empty
         with get () = empty
 
-    /// Creates a ListZipper whose initial position is prior to
-    /// the head element of the given list.
+    /// <summary>Creates a <see cref="ListZipper`1"/> whose initial position is prior to the head element of the given list.</summary>
+    /// <param name="list"></param>
     new (list : 'T list) =
         ListZipper ([], list)
 
-    /// Is the zipper positioned prior to the start of the list?
+    /// <summary>Is the zipper positioned prior to the start of the list?</summary>
     member __.AtStart
         with get () =
             List.isEmpty path
 
-    /// Is the zipper positioned after the end of the list?
+    /// <summary>Is the zipper positioned after the end of the list?</summary>
     member __.AtEnd
         with get () =
             List.isEmpty list
 
-    /// Is the zipper empty?
+    /// <summary>Is the zipper empty?</summary>
     member __.IsEmpty
         with get () =
             List.isEmpty path && List.isEmpty list
 
-    /// The previous element (if any) and current element (if any),
-    /// based on the zipper's current position.
+    /// <summary>The previous element (if any) and current element (if any), based on the zipper's current position.</summary>
     member __.Context
         with get () =
             match path, list with
@@ -66,7 +66,7 @@ type ListZipper<'T> private (path : 'T list, list : 'T list) =
             | prev :: _, current :: _ ->
                 Some prev, Some current
 
-    /// The current element (if any).
+    /// <summary>The current element (if any).</summary>
     member __.Current
         with get () =
             match list with
@@ -74,7 +74,7 @@ type ListZipper<'T> private (path : 'T list, list : 'T list) =
             | current :: _ ->
                 Some current
 
-    /// The previous element (if any).
+    /// <summary>The previous element (if any).</summary>
     member __.Previous
         with get () =
             match path with
@@ -82,64 +82,77 @@ type ListZipper<'T> private (path : 'T list, list : 'T list) =
             | prev :: _ ->
                 Some prev
 
-    /// Moves the zipper backwards by one step.
-    /// No exception is raised if the zipper is already at the start of the list.
+    /// <summary>Moves the zipper backwards by one step.</summary>
+    /// <returns></returns>
+    /// <remarks>No exception is raised if the zipper is already at the start of the list.</remarks>
     member this.MoveBack () =
         match path with
         | [] -> this
         | prev :: path ->
             ListZipper (path, prev :: list)
 
-    /// Moves the zipper forwards by one step.
-    /// No exception is raised if the zipper is already at the end of the list.
+    /// <summary>Moves the zipper forwards by one step.</summary>
+    /// <returns></returns>
+    /// <remarks>No exception is raised if the zipper is already at the end of the list.</remarks>
     member this.MoveNext () =
         match list with
         | [] -> this
         | current :: list ->
             ListZipper (current :: path, list)
 
-    /// Moves the zipper to the start of the list.
-    /// No exception is raised if the zipper is already at the start of the list.
+    /// <summary>Moves the zipper to the start of the list.</summary>
+    /// <returns></returns>
+    /// <remarks>No exception is raised if the zipper is already at the start of the list.</remarks>
     member this.MoveStart () =
         match path with
         | [] -> this
         | prev :: path ->
             (ListZipper (path, prev :: list)).MoveStart ()
 
-    /// Moves the zipper to the end of the list.
-    /// No exception is raised if the zipper is already at the end of the list.
+    /// <summary>Moves the zipper to the end of the list.</summary>
+    /// <returns></returns>
+    /// <remarks>No exception is raised if the zipper is already at the end of the list.</remarks>
     member this.MoveEnd () =
         match list with
         | [] -> this
         | current :: list ->
             (ListZipper (current :: path, list)).MoveEnd ()
 
-    /// Creates a new zipper by removing the current element from the list.
-    /// No exception is raised if the zipper is empty.
+    /// <summary>Creates a new zipper by removing the current element from the list.</summary>
+    /// <returns></returns>
+    /// <remarks>No exception is raised if the zipper is empty.</remarks>
     member this.Remove () =
         match list with
         | [] -> this
         | _ :: list ->
             ListZipper (path, list)
 
+    /// <summary>
     /// Creates a new zipper by inserting the given value prior to the current list element.
     /// If the zipper is empty, the new zipper's list is created from the given value.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
     member this.Insert (value : 'T) =
         ListZipper (path, value :: list)
 
-    /// <summary>
-    /// Creates a new zipper by replacing the current list element with the given value.
+    /// <summary>Creates a new zipper by replacing the current list element with the given value.</summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    /// <remarks>
     /// This is similar to <c>zipper.Remove().Insert(value)</c> except that when the zipper
     /// is empty, <c>zipper.Update(value)</c> returns an empty zipper.
-    /// </summary>
+    /// </remarks>
     member this.Update (value : 'T) =
         match list with
         | [] -> this
         | _ :: list ->
             ListZipper (path, value :: list)
 
-    /// Returns a new list whose elements are the same as those in the zipper,
-    /// and also in the same order as those in the zipper.
+    /// <summary>
+    /// Returns a new list whose elements are the same as those in the zipper, and also in the same order as those in the zipper.
+    /// </summary>
+    /// <returns></returns>
     member __.ToList () : 'T list =
         // Same as ListZipper.MoveStart(), except here we return the list
         // once we've reached the start of the list.
@@ -149,7 +162,7 @@ type ListZipper<'T> private (path : 'T list, list : 'T list) =
             (ListZipper (path, prev :: list)).ToList ()
 
 
-/// Functional operators related to the ListZipper<_> type.
+/// <summary>Functional operators related to the <see cref="ListZipper`1"/> type.</summary>
 [<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module ListZipper =
     /// The empty zipper value.
@@ -172,8 +185,7 @@ module ListZipper =
     let inline atEnd (zipper : ListZipper<'T>) : bool =
         zipper.AtEnd
 
-    /// The previous element (if any) and current element (if any),
-    /// based on the zipper's current position.
+    /// The previous element (if any) and current element (if any), based on the zipper's current position.
     [<CompiledName("Context")>]
     let inline context (zipper : ListZipper<'T>) : 'T option * 'T option =
         zipper.Context
