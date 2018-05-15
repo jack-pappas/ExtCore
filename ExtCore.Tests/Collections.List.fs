d7776e74af657434a931ae4ab4d24302622edabc
Sachin Shah <sachin_shah@redmane.com>
2018-05-15 17:57:38 -0500

    Added initial FsCheck using alternate implementation.

--- ExtCore.Tests/Collections.List.fs
+++ ExtCore.Tests/Collections.List.fs
@@ -345,7 +345,7 @@ let exactlyOne () : unit =
     assertRaises<System.ArgumentException> <| fun () ->
         List.exactlyOne []
         |> ignore
-    
+
     // Test case for a single-element list.
     ["Hello World!"]
     |> List.exactlyOne
@@ -405,6 +405,26 @@ let difference () : unit =
     List.difference ([6; 7; 8; 9; 0] @ [1; 2; 3; 4; 5; 6; 7; 8; 9; 0]) [6; 7; 8; 9; 0]
     |> Collection.assertEqual [1; 2; 3; 4; 5; 6; 7; 8; 9; 0]
 
-open FsCheck
+
+
+module FsCheck =
+    open FsCheck
+
+    [<Test>]
+    let ``List difference random testing``() =
+        assertProp "List difference random testing" <| fun (m:int list) (n:int list) ->
+            if isNull m || isNull n then
+                assertRaises<System.ArgumentNullException>
+                    (fun () -> List.difference m n |> ignore)
+            else ()
+
+            let removeFirst p xs =
+                match List.tryFindIndex p xs with
+                | Some i -> (List.take i xs |> fst ) @ (List.skip (i+1) xs)
+                | None -> xs
+
+            let o = List.fold (fun state el -> removeFirst ((=) el) state) m n
+
+            List.difference m n = o
 
 (* TODO : Implement randomized tests with FsCheck. *)
